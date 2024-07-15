2d2e078dbe548a6f5f4993fe21f36e5eedaaadd7
Elena Ilkevich <166912893+5865bk@users.noreply.github.com>
2024-07-15 10:20:12 +0300

    добавлено: иквалс и хешкод в модель

--- src/main/java/web/model/Car.java
+++ src/main/java/web/model/Car.java
@@ -1,5 +1,7 @@
 package web.model;
 
+import java.util.Objects;
+
 public class Car {
     private String model;
     private int series;
@@ -34,6 +36,19 @@ public class Car {
 
     public void setYear(int year) {
         this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return series == car.series && year == car.year && Objects.equals(model, car.model);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(model, series, year);
     }
 
     @Override
